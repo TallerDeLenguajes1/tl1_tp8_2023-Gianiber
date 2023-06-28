10e656a46c4c5362fae40663b02dff82e0e5dfeb
Gianfranco Bernachi <gianfrancobernachi@gmail.com>
2023-06-27 23:11:43 -0300

    Directorio

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using ListadoDeTareas;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 
-string respuesta = "";
+/*string respuesta = "";
 string descripcion;
 int i = 0;
 List<Tarea> ListaTareas = new List<Tarea>();
@@ -40,7 +41,7 @@ foreach (Tarea tareaX in ListaTareas)
     Console.WriteLine(tareaX.Id + " - " + tareaX.Descripcion + " - " + tareaX.Estado + " - " + tareaX.Duracion);
 }
 
-/*Console.WriteLine("Que tarea desea buscar?");
+Console.WriteLine("Que tarea desea buscar?");
 descripcion = Console.ReadLine();
 int bandera = 0;
 
@@ -56,7 +57,7 @@ foreach (Tarea tareaX in ListaTareas)
 if (bandera == 0)
 {
     Console.WriteLine("No se encontró la tarea requerida");
-}*/
+}
 
 string ubicacion = @"C:\RepoTaller\tl1_tp8_2023-Gianiber\empleado.txt";
 int tiempo = 0;
@@ -87,4 +88,29 @@ else
     }
 
     sr.WriteLine("Empleado 0 - Trabajó " + tiempo + "hs");
+}
+*/
+
+string? ruta;
+Console.WriteLine("Ingrese el directorio que desea analizar:");
+ruta = Console.ReadLine();
+
+if (Directory.Exists(ruta))
+{
+    string[] archivos = Directory.GetFiles(ruta);
+    string csvPath = Path.Combine(ruta, "index.csv");
+    StringBuilder csvBuilder = new StringBuilder();
+    //using FileStream fs = File.Create(String.Concat(ruta, @"\index.csv"));
+    //using StreamWriter sr = new StreamWriter(fs);
+    for (int i = 0; i < archivos.Length; i++)
+    {
+        string archivo = archivos[i];
+        string nombre = Path.GetFileNameWithoutExtension(archivo);
+        string ext = Path.GetExtension(archivo);
+        //sr.WriteLine(i + " - " + nombre + " - " + ext + ";");
+        string linea = $"{i + 1},{nombre},{ext}";
+        csvBuilder.AppendLine(linea);
+        Console.WriteLine(linea);
+    }
+    File.WriteAllText(csvPath, csvBuilder.ToString());
 }
